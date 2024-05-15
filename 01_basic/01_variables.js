1e13b5efb7d840e6f535ece13e08f19673d4e257
Jayanta Mandal <49152752+jayanta-mandal@users.noreply.github.com>
2024-05-15 10:38:14 +0000

    datatypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12565465
+let accountEmail = "jayantamandal011@gmail.com"
+var accountPass = "12345"
+
+accountCity = "Kolkata"
+
+
+// accountId = 2 // not allowed as it is const
+
+accountEmail = "jayantamandal@gmail.com"
+accountPass = "987576764"
+accountCity = "Bankura"
+
+console.table([accountId,accountEmail, accountPass, accountCity  ])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3 + 3)
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => not assign any value
+// symbol => unique
+
+
+//object
+
+console.log(typeof "new")
+console.log(typeof null)
+console.log(typeof undefined)
