1085ee80837640cb17e6eac2a12fef3e24c9c46b
Jayanta Mandal <49152752+jayanta-mandal@users.noreply.github.com>
2024-08-07 08:51:12 +0000

    Conversions

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -1,7 +1,6 @@
 "use strict"; //treat all js code as newer version
 
 // alert(3 + 3)
-
 // number => 2 to power 53
 // bigint
 // string => ""
@@ -9,8 +8,6 @@
 // null => standalone value
 // undefined => not assign any value
 // symbol => unique
-
-
 //object
 
 console.log(typeof "new")
---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNunber = Number(score);
+console.log(valueInNunber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+//********** Operations ***********
+
+let value = 3;
+let negValue = -value;
+
+console.log(negValue);
+
+str1 = "hello ",
+str2 = "Jayanta"
+str3 = str1 + str2;
+console.log(str3)
+
+console.log("1" + 2 + 2)
+console.log(2 + 3 + "2")
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
